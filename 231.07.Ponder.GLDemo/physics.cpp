45bc57429ce6ddac675d84bcbe042faed5950aa1
Njjeppson <njshifty@gmail.com>
2023-02-16 20:38:18 -0700

    Update physics.cpp

--- 231.07.Ponder.GLDemo/physics.cpp
+++ 231.07.Ponder.GLDemo/physics.cpp
@@ -5,9 +5,19 @@ class Physics
 {
 public:
 
-	double heightAboveEarth(double x, double y, double r) {
-		double h = sqrt((x * x) + (y * y)) - r;
+	double gravity(double h) {
+		double gh = 9.80665 * (6378000 / (6378000 + h));
+		return gh;
+	}
+
+	double heightAboveEarth(double x, double y) {
+		double h = sqrt((x * x) + (y * y)) - 6378000;
 		return h;
+	}
+
+	double directionOfGravityPull(double xs, double ys) {
+		double d = atan2(0 - xs, 0 - ys);
+		return d;
 	}
 
 };
